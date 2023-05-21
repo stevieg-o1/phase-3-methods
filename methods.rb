757ed25e1cfaf2730eb984e780b23ddc34b2e330
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-05-21 15:37:19 +0300

    completed lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+     end
+    
+     def greet name
+        puts "Hello, #{name}!"
+     end
+    
+     def greet_with_default name ="Hello, programmer!"
+     puts "Hello, #{name}!"
+     end
+    
+     def add(num1 ,num2)
+    return  num1 +num2
+     end
+    
+     def halve data 
+       if data.is_a?Integer
+          return data/2
+       else
+          return nil
+       end 
+    end
